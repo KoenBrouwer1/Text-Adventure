534f6f372f0eeaa1b0de9cfcacde0439531e7190
Koen <k.brouwer07@student.alfa-college.nl>
2025-03-10 11:45:02 +0100

    quick change

---
+++ src/Player.cs
@@ -0,0 +1,51 @@
+
+
+class Player
+{
+
+    // auto property
+    public Room CurrentRoom { get; set; }
+    private int health = 100;
+
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+
+    // constructor
+    public Player()
+    {
+
+        CurrentRoom = null;
+        health = 100;
+        Inventory inventory = new Inventory(10);
+    }
+
+    // methods
+    public int Damage(int amount)
+    {
+        health -= amount;
+        return health;
+    }
+    // player loses some health
+    public int Heal(int amount)
+    {
+        health += amount;
+        return health;
+    }
+    // player's health restores
+    public bool IsAlive()
+    {
+        if (health <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
+//damage staat in de game file in rij 186 :)
